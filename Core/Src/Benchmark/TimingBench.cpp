681d2a80a17bbf9353821d4227dc9368aa103836
Revalioli <elinor.montmasson@gmail.com>
2022-06-16 15:20:16 +0200

    Add Bench virtual class and use it as TimingBench base class, add unit choice for TimingBench results

--- Core/Src/Benchmark/TimingBench.cpp
+++ Core/Src/Benchmark/TimingBench.cpp
@@ -1,13 +1,20 @@
 #include "TimingBench.h"
 
+#include <algorithm>
+#include <iostream>
 #include "main.h"
 
-TimingBench::TimingBench(void (*function)(void), TIM_HandleTypeDef * timer, int nbAttempts) : tim(timer), bench_fun(function), nbAttempts(nbAttempts), result(0) {
+TimingBench::TimingBench(void (*fun)(void), TIM_HandleTypeDef * timer, int nbAttempts, TimeUnit unit, float unitMultiplier)
+		: Bench(fun), tim(timer), timerUnit(unit), timerMultiplier(unitMultiplier), nbAttempts(nbAttempts), result(-1)
+{
 	HAL_TIM_Base_Stop(timer);
 }
 
-int TimingBench::startBench(){
+void TimingBench::startBench(){
 
+	this->state = BenchState::Working;
+
+	// Reset results
 	this->result = 0;
 	this->attemptResults.clear();
 
@@ -16,7 +23,7 @@ int TimingBench::startBench(){
 		__HAL_TIM_SET_COUNTER(this->tim, (int32_t)(0x0));
 		HAL_TIM_Base_Start(this->tim);
 
-		(*this->bench_fun)();
+		(*this->benchFunction)();
 
 		HAL_TIM_Base_Stop(tim);
 		uint32_t raw = (uint32_t)(__HAL_TIM_GET_COUNTER(this->tim));
@@ -25,8 +32,41 @@ int TimingBench::startBench(){
 		this->result += timing;
 	}
 
+	// Compute final result
 	this->result = this->result / this->nbAttempts;
 
-	return this->result;
+	this->state = BenchState::Ok;
+}
+
+int TimingBench::getResult() { return this->result; }
+
+
+void TimingBench::printResult(){
+
+	if(this->state == BenchState::Ok){
+
+		int maxTime = *std::max_element(this->attemptResults.cbegin(), this->attemptResults.cend());
+		int minTime = *std::min_element(this->attemptResults.cbegin(), this->attemptResults.cend());
+
+		// Choose unit string
+		char * unitStr;
+
+		switch(this->timerUnit){
+			case(TimeUnit::Seconds) : unitStr = "s"; break;
+			case(TimeUnit::Milliseconds) : unitStr = "ms"; break;
+			case(TimeUnit::Microseconds) : unitStr = "us"; break;
+		}
+
+
+		std::cout << "***** Last TimingBench results *****" << std::endl;
+
+		std::cout << "=== T_min : " << (minTime * this->timerMultiplier) << ' ' << unitStr << std::endl;
+		std::cout << "=== T_avg : " << (this->result * this->timerMultiplier) << ' ' << unitStr << std::endl;
+		std::cout << "=== T_max : " << (maxTime * this->timerMultiplier) << ' ' << unitStr << std::endl;
+
+	}
 
 }
+
+
+
