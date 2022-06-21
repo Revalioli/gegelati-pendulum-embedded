8beced97a8916507d8b9264840ce5c21fcf93d27
Revalioli <elinor.montmasson@gmail.com>
2022-06-21 11:56:48 +0200

    Change sampling frequency for current monitors

--- Core/Src/Peripherals/tim.c
+++ Core/Src/Peripherals/tim.c
@@ -86,7 +86,7 @@ void MX_TIM7_Init(void)
   htim7.Instance = TIM7;
   htim7.Init.Prescaler = 8399;
   htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim7.Init.Period = 9;
+  htim7.Init.Period = 29;
   htim7.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim7) != HAL_OK)
   {
