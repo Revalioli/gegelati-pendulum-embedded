dbf65f0ad1508d4ba2ff5404bc47f2c73f941cd8
Revalioli <elinor.montmasson@gmail.com>
2022-07-15 20:44:18 +0200

    Add README.md, add instructions for third-party software installation, add install_packages script

---
+++ install_packages.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# Ruby gems
+echo '=====[ Installing Ruby gems ]====='
+gem install serialport
+
+# Python packages
+echo '=====[ Installing Python packages ]====='
+pip install numpy
+pip install matplotlib
+
+# Julia packages
+echo '=====[ Installing Julia packages ]====='
+
+echo TODO
+# TODO
