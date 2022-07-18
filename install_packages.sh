7cd722ed5c7a7372af5c614bbbe19774c4682bb7
Revalioli <elinor.montmasson@gmail.com>
2022-07-18 10:24:40 +0200

    Add install_packages.sh, move script to suddir, improved READLE.md dependencies section

--- install_packages.sh
+++ install_packages.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/bash
 
 # Ruby gems
-echo '=====[ Installing Ruby gems ]====='
-gem install serialport
+echo -e "\e[1;92m=====[ Installing Ruby gems ]=====\e[0m"
+sudo gem install serialport
 
 # Python packages
-echo '=====[ Installing Python packages ]====='
+echo -e "\e[1;92m=====[ Installing Python packages ]=====\e[0m"
 pip install numpy
 pip install matplotlib
 
 # Julia packages
-echo '=====[ Installing Julia packages ]====='
-
-echo TODO
-# TODO
+echo -e "\e[1;92m====[ Installing Julia packages ]=====\e[0m"
+cd ./scripts
+./install_julia_packages.jl
