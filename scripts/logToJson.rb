882b96c593110a6e3a1d79c510fdc452e39445b9
Montmasson Emmanuel <emmanuel.montmasson@insa-rennes.fr>
2022-07-28 11:39:19 +0200

    Merge energy_summary.md data in energy_data.json, remove compute_energy_summary.jl as it is now done by logToJson.rb

--- scripts/logToJson.rb
+++ scripts/logToJson.rb
@@ -1,7 +1,12 @@
-#!/usr/bin/ruby
-
 require 'json'
 
+##
+#   Parse a energy measurment log file and export data in a json file, with a few other information.
+#
+#   **logPath** path of the parsed .log file.
+#   **jsonPath** path of the json file to be written.
+#
+#   **Return :** the Hash used to generate the json file.
 def logToJson(logPath, jsonPath)
     # Format the log file from a energy measurment to a json file, easily parsable by any language.
 
@@ -15,12 +20,13 @@ def logToJson(logPath, jsonPath)
     dataLine = /(#{integerRegex})\t(#{floatRegex})\t(#{floatRegex})/
     paramLine = /dataTimeUnit : (#{textRegex})\tdataTimerMultiplier : (#{floatRegex})\tStartAngle : (#{floatRegex})\tStartVelocity : (#{floatRegex})/
     headerLine = /Step\tCurrent\tPower/
-    inferenceTimingLine = /\=\=\= T_(#{textRegex}) : (\d* #{textRegex})/
+    executionTimingLine = /\=\=\= T_(#{textRegex}) : (\d* #{textRegex})/
 
 
-    jsonHash = {"metadata" => {}, "step" => [], "current" => [], "power" => [] }
+    jsonHash = {"metadata" => {}, "summary" => {}, "step" => [], "current" => [], "power" => [] }
     skipLine = false
 
+    # Parsing energy.log
     File.open(logPath).each_line { |line|
         
         if skipLine
@@ -45,14 +51,55 @@ def logToJson(logPath, jsonPath)
                 jsonHash["step"] << $1.to_i
                 jsonHash["current"] << $2.to_f
                 jsonHash["power"] << $3.to_f
-            when inferenceTimingLine
-                jsonHash["metadata"]["inferenceT#{$1}"] = $2
+            when executionTimingLine
+                jsonHash["summary"]["executionT#{$1}"] = $2
             end
 
         end
 
-        }.close
+    }.close
 
-        File.write(jsonPath, JSON.pretty_generate(jsonHash))
+
+    # Remove invalid data
+
+    toRemove = []
+
+    jsonHash["step"].each.with_index { |s, idx|
+        toRemove << idx if s < 0
+    }
+
+    toRemove.each { |delete_index|
+        jsonHash["step"].delete_at(delete_index)
+        jsonHash["current"].delete_at(delete_index)
+        jsonHash["power"].delete_at(delete_index)
+    }
+
+    # Compute additional information
+
+    jsonHash["summary"]["averageCurrent"] = jsonHash["current"].sum / jsonHash["current"].length
+    jsonHash["summary"]["averagePower"] = jsonHash["power"].sum / jsonHash["power"].length
+    
+    timeUnit = jsonHash["metadata"]["dataTimeUnit"]
+    timeMultiplier = jsonHash["metadata"]["dataTimeMultiplier"]
+
+    measureStepTime = timeMultiplier  # In seconds
+
+    case timeUnit
+    when "ms"
+        measureStepTime = timeMultiplier / 10**3
+    when "us"
+        measureStepTime = timeMultiplier / 10**6
+    when "s"    # Do nothing
+    else
+        puts "Unknown time unit, default to s"
+    end
+
+    jsonHash["summary"]["totalEnergy"] = jsonHash["power"].sum { |p| p * measureStepTime } # In J
+
+
+
+    File.write(jsonPath, JSON.pretty_generate(jsonHash))
+
+    jsonHash    # Return to caller
 
 end
